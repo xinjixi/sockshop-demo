47a8f9b9f37c6eb9376ce76841475a04b0d3542d
coliu19 <coliu@cisco.com>
2022-05-17 17:33:30 +0800

    perfect catalogue api

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 set -x
 
-apiregistryctl service uploadspec v0.0-rev1/catalogue.json -s catalogue --version 0.0 --revision 1 --debug
+# apiregistryctl service uploadspec v0.0-rev1/catalogue.json -s catalogue --version 0.0 --revision 1 --debug
 apiregistryctl service uploadspec v0.0-rev2/catalogue.json -s catalogue --version 0.0 --revision 2 --debug
+
+
+cp v0.0-rev2/catalogue.json openapi/
+git diff
+
+git add openapi/catalogue.json
+git commit -m "perfect catalogue api"
+git push origin master
