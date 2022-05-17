6f683f4ef83325fd52cf5106e22816eecd2f0289
coliu19 <coliu@cisco.com>
2022-05-17 18:42:23 +0800

    fix script

--- 1-prepare.sh
+++ 1-prepare.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 set -x
 
-# apiregistryctl service uploadspec v0.0-rev1/catalogue.json -s catalogue --version 0.0 --revision 1 --debug
+host=https://devnet-testing.cisco.com
+
+service=catalogue
+
+apiregistryctl service delete "$service" --debug
+
+printf -v payload '{ "organization_id": "DevNet", "product_tag": "Sock Shop", "name_id": "%s", "title": "demo %s", "description": "%s API", "contact": {} }' "$service" "$service" "$service"
+apiregistryctl -H "$host" service create --data "$payload" --debug
+
+apiregistryctl -H "$host" service list | grep catalogue
+
+apiregistryctl service uploadspec v0.0-rev1/catalogue.json -s catalogue --version 0.0 --revision 1 --debug
 apiregistryctl service uploadspec v0.0-rev2/catalogue.json -s catalogue --version 0.0 --revision 2 --debug
 
 
--- 4-fix-violations.sh
+++ 4-fix-violations.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 set -x
 
-cp v0.1-rev1/catalogue.json openapi/
+cp v0.1-rev2/catalogue.json openapi/
 git diff
 
 git add openapi/catalogue.json
 git commit -m "fix catalogue api"
 git push origin master
-
-sleep 2s
-./trigger-ci-upload.sh v0.1-1
